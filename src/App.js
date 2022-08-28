a24afb4af53f3d8d289c4c4388419acf32c37b3c
SolidSok <solidsok@gmail.com>
2022-08-28 12:53:12 -0700

    test updates for scenario 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import EventList from './EventList';
+import CitySearch from './CitySearch';
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <CitySearch />
+      <EventList />
+    </div>
+  );
 }
 
 export default App;
