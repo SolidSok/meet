a7099adf53d2fa46e4f945af65a01d730d08416a
SolidSok <90538446+SolidSok@users.noreply.github.com>
2022-08-31 01:07:41 -0700

    NumberOfEvents initial testing

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 
 class NumberOfEvents extends Component {
+  state = { renderNumber: 32 };
+  handleInputChanged = event => {
+    this.setState({
+      renderNumber: event.target.value,
+    });
+  };
   render() {
     return (
       <div className="">
-        <input className="event-amount"></input>
+        <p className="event-amount-header">
+          Number of events displayed: {this.state.renderNumber}
+        </p>
+        <input
+          className="event-amount"
+          value={this.state.renderNumber}
+          onChange={this.handleInputChanged}
+        />
       </div>
     );
   }
--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -6,8 +6,32 @@ describe('NumberOfEvents /> component', () => {
   let NumberOfEventsWrapper;
   beforeAll(() => {
     NumberOfEventsWrapper = shallow(<NumberOfEvents />);
+    NumberOfEventsWrapper.setState({
+      renderNumber: 32,
+    });
   });
-  test('render text input', () => {
+  test('render input', () => {
     expect(NumberOfEventsWrapper.find('.event-amount')).toHaveLength(1);
+  });
+  test('render default number as 32', () => {
+    expect(NumberOfEventsWrapper.find('.event-amount').prop('value')).toBe(32);
+  });
+  test('render event-amount-header', () => {
+    expect(NumberOfEventsWrapper.find('.event-amount-header')).toHaveLength(1);
+  });
+  test('event-amount-header rendered correctly', () => {
+    expect(NumberOfEventsWrapper.find('.event-amount-header').text()).toBe(
+      `Number of events displayed: ${NumberOfEventsWrapper.state(
+        'renderNumber'
+      )}`
+    );
+  });
+  test('change state when text input changes', () => {
+    NumberOfEventsWrapper.setState({
+      renderNumber: '32',
+    });
+    const eventObject = { target: { value: 8 } };
+    NumberOfEventsWrapper.find('.event-amount').simulate('change', eventObject);
+    expect(NumberOfEventsWrapper.state('renderNumber')).toBe(8);
   });
 });
