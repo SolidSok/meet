66ad1bf9c6a576b0857f6ca4c2186bb70ec072f9
SolidSok <solidsok@gmail.com>
2022-09-03 23:33:52 -0700

    nprogress

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
 import { mockData } from './mock-data';
 import axios from 'axios';
+import NProgress from 'nprogress';
 
 export const getAccessToken = async () => {
   const accessToken = localStorage.getItem('access_token');
@@ -20,6 +21,23 @@ export const getAccessToken = async () => {
   }
   return accessToken;
 };
+const getToken = async code => {
+  const encodeCode = encodeURIComponent(code);
+  const { access_token } = await fetch(
+    'https://hi9boatpvf.execute-api.us-west-1.amazonaws.com/dev/api/token' +
+      '/' +
+      encodeCode
+  )
+    .then(res => {
+      return res.json();
+    })
+    .catch(error => error);
+
+  access_token && localStorage.setItem('access_token', access_token);
+
+  return access_token;
+};
+
 const checkToken = async accessToken => {
   const result = await fetch(
     `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
@@ -29,6 +47,7 @@ const checkToken = async accessToken => {
 
   return result;
 };
+
 export const getEvents = async () => {
   NProgress.start();
 
@@ -36,7 +55,6 @@ export const getEvents = async () => {
     NProgress.done();
     return mockData;
   }
-
   const token = await getAccessToken();
 
   if (token) {
@@ -53,6 +71,20 @@ export const getEvents = async () => {
     }
     NProgress.done();
     return result.data.events;
+  }
+};
+
+const removeQuery = () => {
+  if (window.history.pushState && window.location.pathname) {
+    var newurl =
+      window.location.protocol +
+      '//' +
+      window.location.host +
+      window.location.pathname;
+    window.history.pushState('', '', newurl);
+  } else {
+    newurl = window.location.protocol + '//' + window.location.host;
+    window.history.pushState('', '', newurl);
   }
 };
 
